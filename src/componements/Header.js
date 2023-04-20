c759a9ae1d0f5046ec362781c186e7e0c5161b01
name <p.etienne.bernard@gmail.com>
2023-04-20 11:08:32 +0530

    Initial
    Signed-off-by: name <p.etienne.bernard@gmail.com>

---
+++ src/componements/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Todo liste ⛩️</h1>
+        </div>
+    );
+};
+export default Header  
